c7eb10817069822d2aac9efe5f9cd7df49d1875e
fandi a <fandiiardy@gmail.com>
2024-11-25 10:26:56 +0700

    Add Some File

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Home from './Pages/Home'
+import Result from './Pages/Result'
+import BuyCredit from './Pages/BuyCredit'
 
 const App = () => {
   return (
     <div>
-      
+      <Home/>
+      <Result/>
+      <BuyCredit/>
     </div>
   )
 }
