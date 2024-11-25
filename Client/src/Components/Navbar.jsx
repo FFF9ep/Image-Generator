c03b93a7db43851d515e6fab3cfaad0917b41d60
fandi a <fandiiardy@gmail.com>
2024-11-25 11:12:30 +0700

    Fix Bugs

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
+import { AppContext } from '../Context/AppContext';
 
 const Navbar = () => {
 
-    
+    const {user} = useContext(AppContext);
 
     const navigate = useNavigate();
 
