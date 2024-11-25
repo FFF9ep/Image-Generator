8896a246751fb6982493ab4b02e82d2d3fd0bcab
fandi a <fandiiardy@gmail.com>
2024-11-25 10:42:27 +0700

    Update Nav

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div>
-            <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/>
+            <Link to='/'>
+                <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/>
+            </Link>
+
+            <div>
+                
+            </div>
         </div>
     )
 }
