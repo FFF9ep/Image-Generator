bf1aa8cccb4f87c3a0af00ee57de2c2054a62f19
fandi a <fandiiardy@gmail.com>
2024-11-25 10:59:54 +0700

    Update Navbar

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
 
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(true);
 
     const navigate = useNavigate();
 
@@ -16,7 +16,21 @@ const Navbar = () => {
 
             <div>
                 {user ? 
-                <div></div> 
+                <div className='flex items-center gap-2 sm:gap-3'>
+                    <button className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
+                        <img className='w-5' src={assets.credit_star} alt="" />
+                        <p>Credit Left : 50</p>
+                    </button>
+                    <p>Hi, Fandi!</p>
+                    <div className='relative group'>
+                        <img src={assets.profile_icon} className='w-10 drop-shadow' alt="" />
+                        <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                            <ul>
+                                <li>Logout</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div> 
                 :
                 <div className='flex items-center gap-2 sm:gap-5'>
                     <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
