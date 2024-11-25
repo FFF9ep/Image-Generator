4810ca399e2a2d0a5c0bfe9ef78280f818181d02
fandi a <fandiiardy@gmail.com>
2024-11-25 10:31:29 +0700

    Update Route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Home from './Pages/Home'
 import Result from './Pages/Result'
 import BuyCredit from './Pages/BuyCredit'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Home/>
-      <Result/>
-      <BuyCredit/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
     </div>
   )
 }
