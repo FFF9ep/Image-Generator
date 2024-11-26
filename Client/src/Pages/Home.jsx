ce88f6613bbaf07e9be16844b62d50a6bda06c95
fandi a <fandiiardy@gmail.com>
2024-11-26 12:56:27 +0700

    Update

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header'
 import Steps from '../Components/Steps'
+import Description from '../Components/Description'
 
 const Home = () => {
     return (
         <div>
             <Header/>
             <Steps/>
+            <Description/>
         </div>
     )
 }
