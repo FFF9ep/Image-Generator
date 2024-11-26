cd4c295892f18772bd7e1e6ea765f4d90a3714bb
fandi a <fandiiardy@gmail.com>
2024-11-26 12:52:23 +0700

    Bugs

--- Client/src/Components/Steps.jsx
+++ Client/src/Components/Steps.jsx
@@ -10,7 +10,7 @@ const Steps = () => {
             <div className='space-y-4 w-full max-w-3xl text-sm'>
                 {stepsData.map((item, index) => (
                     <div key={index} 
-                    className='items-center gap-4 p-5 px-8 bg-white/20 shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300'>
+                    className='items-center gap-4 p-5 px-8 bg-white/20 shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300 rounded-lg'>
                         <img width={40} src={item.icon} alt="" />
                         <div>
                             <h2 className='text-xl font-medium'>{item.title}</h2>
