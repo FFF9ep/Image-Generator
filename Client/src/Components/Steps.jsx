c74905ac73edc556d3739128ce910ea62fbc4f94
fandi a <fandiiardy@gmail.com>
2024-11-26 12:44:10 +0700

    Update

---
+++ Client/src/Components/Steps.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Steps = () => {
+    return (
+        <div>
+            <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>How it Works?</h1>
+            <p className='text-lg text-gray-600 mb-8'>Transform Word Into Stunning Images</p>
+        </div>
+    )
+}
+
+export default Steps
