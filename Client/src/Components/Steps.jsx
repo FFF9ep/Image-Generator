be24628e8c8512a40595765348de5c93d5edad69
fandi a <fandiiardy@gmail.com>
2024-11-26 12:44:51 +0700

    Update

--- Client/src/Components/Steps.jsx
+++ Client/src/Components/Steps.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { stepsData } from '../assets/assets'
 
 const Steps = () => {
     return (
         <div>
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>How it Works?</h1>
             <p className='text-lg text-gray-600 mb-8'>Transform Word Into Stunning Images</p>
+
+            <div>
+                {stepsData}
+            </div>
         </div>
     )
 }
