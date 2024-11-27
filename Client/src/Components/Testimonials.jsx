b19000470aec6ea44dcb217a8a24d9145ac456ca
fandi a <fandiiardy@gmail.com>
2024-11-27 19:16:07 +0700

    Update

--- Client/src/Components/Testimonials.jsx
+++ Client/src/Components/Testimonials.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { testimonialsData } from '../assets/assets'
 
 const Testimonials = () => {
     return (
         <div className='flex flex-col justify-center items-center my-20 py-12'>
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Testimonials</h1>
             <p className='text-gray-500 mb-12'>What our user saying</p>
+
+            <div className='flex flex-wrap gap-6'>
+                {testimonialsData.map((testimonials, index) => (
+                    <div key={index}>
+                        
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
