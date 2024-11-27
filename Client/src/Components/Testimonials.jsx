6bb22f71de461c00660b257b383a527f70122e4f
fandi a <fandiiardy@gmail.com>
2024-11-27 19:14:16 +0700

    Testimonials Update

--- Client/src/Components/Testimonials.jsx
+++ Client/src/Components/Testimonials.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Testimonials = () => {
     return (
-        <div>
-            
+        <div className='flex flex-col justify-center items-center my-20 py-12'>
+            <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Testimonials</h1>
+            <p className='text-gray-500 mb-12'>What our user saying</p>
         </div>
     )
 }
