e831f02acf11eed2de0652fff5174f240939a014
fandi a <fandiiardy@gmail.com>
2024-11-29 12:16:36 +0700

    Update Result

--- Client/src/Pages/Result.jsx
+++ Client/src/Pages/Result.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { assets } from "../assets/assets";
 
 const Result = () => {
     return (
         <div>
-            
+            <div className='relative'>
+                <img src={assets.sample_img_1} alt="" className='max-w-sm rounded'/>
+                <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]'/>
+            </div>
         </div>
     )
 }
