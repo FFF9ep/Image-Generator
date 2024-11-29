ed40aff948906e826b244fb6e6b95daec045ebd3
fandi a <fandiiardy@gmail.com>
2024-11-29 14:27:05 +0700

    Fixing Bugs

--- Client/src/Pages/Result.jsx
+++ Client/src/Pages/Result.jsx
@@ -3,8 +3,8 @@ import { assets } from "../assets/assets";
 
 const Result = () => {
     return (
-        <form>
-        <div className='flex flex-col min-h-[90vh] justify-center items-center'>
+        <form className='flex flex-col min-h-[90vh] justify-center items-center'>
+        <div>
             <div className='relative'>
                 <img src={assets.sample_img_1} alt="" className='max-w-sm rounded'/>
                 <span className=' absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]'/>
