6b3db97e381cdcd99fffe7e2ff887be3efce0f28
fandi a <fandiiardy@gmail.com>
2024-11-30 23:29:59 +0700

    Update

--- Client/src/Pages/BuyCredit.jsx
+++ Client/src/Pages/BuyCredit.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BuyCredit = () => {
     return (
         <div className='min-h-[80vh] text-center pt-14 mb-10'>
-            <button>Our Plans</button>
+            <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
             <h1>Choose the plan</h1>
         </div>
     )
