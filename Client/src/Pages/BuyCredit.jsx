07744793fc8c5de5cd72e2b4f59a0c5f74f99420
fandi a <fandiiardy@gmail.com>
2024-12-01 21:52:05 +0700

    Styling

--- Client/src/Pages/BuyCredit.jsx
+++ Client/src/Pages/BuyCredit.jsx
@@ -15,7 +15,9 @@ const BuyCredit = () => {
                         <img width={40} src={assets.logo_icon} alt="" />
                         <p className='mt-3 mb-1 font-semibold'>{item.id}</p>
                         <p className='text-sm'>{item.desc}</p>
-                        <p className='mt-6'>${item.price} / {item.credits} credits</p>
+                        <p className='mt-6'>
+                            <span className='text-3xl font-medium'>${item.price} </span>/ {item.credits} credits
+                        </p>
                     </div>
                 ))}
             </div>
