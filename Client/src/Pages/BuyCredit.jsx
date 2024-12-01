5a438eea3bcdd4c4f27da974acf4268cf8d7ba7c
fandi a <fandiiardy@gmail.com>
2024-12-01 21:57:02 +0700

    Update Context

--- Client/src/Pages/BuyCredit.jsx
+++ Client/src/Pages/BuyCredit.jsx
@@ -23,7 +23,7 @@ const BuyCredit = () => {
                             <span className='text-3xl font-medium'>${item.price} </span>/ {item.credits} credits
                         </p>
                         <button className='w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52'>
-                            Get Started
+                            {user ? 'Purchase' : 'Get Started'}
                         </button>
                     </div>
                 ))}
