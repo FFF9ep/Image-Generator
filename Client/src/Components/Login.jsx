4f64efde7d435cdee3fb791c7d1f6412c0f166b0
fandi a <fandiiardy@gmail.com>
2024-12-03 18:04:44 +0700

    h1

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
       
     <form className='relative bg-white p-10 rounded-xl text-slate-500'>
-        <h1>Sign Up</h1>
+        <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
         <p>Welcomeback! Please sign in to continue</p>
 
         <div>
