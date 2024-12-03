5802539791839cb371133ff5db5ffd183734d155
fandi a <fandiiardy@gmail.com>
2024-12-03 18:54:00 +0700

    Fixing bugs

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -9,11 +9,11 @@ const Login = () => {
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
         <p className='text-sm'>Welcomeback! Please sign in to continue</p>
 
-        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
             <img src={assets} alt="" />
             <input type="text" className='outline-none text-sm' placeholder='Full Name' required />
         </div>
-        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
             <img src={assets.email_icon} alt="" />
             <input type="email" className='outline-none text-sm' placeholder='Email id' required />
         </div>
