83e20324e27bc4da9c52bd7d8e28d7ac0ecda48f
fandi a <fandiiardy@gmail.com>
2024-12-04 12:43:56 +0700

    fixing bugs

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -23,14 +23,14 @@ const Header = () => {
             </motion.div>
 
             <motion.h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn Text to <span className='text-blue-600'
-            initial= {{ opacity: 0 }}
-            animate= {{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             duration={{ delay: 0.4, duration: 2 }}
             >Image</span>, in seconds</motion.h1>
 
             <motion.p className='text-center max-w-xl mx-auto mt-5'
-            initial= {{ opacity: 0, y: 20 }}
-            animate= {{ opacity: 0, y: 0 }}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.6, duration: 0.8 }}
             >Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and wathc the magic engine happen!.</motion.p>
 
