484304451bade5c6038466adab592be8338d8822
fandi a <fandiiardy@gmail.com>
2024-12-05 18:37:07 +0700

    Update

--- Client/src/Components/Steps.jsx
+++ Client/src/Components/Steps.jsx
@@ -5,7 +5,10 @@ import { motion } from 'motion/react'
 const Steps = () => {
     return (
         <motion.div
-        
+        initial= {{ opacity: 0.2, y:100 }}
+        transition= {{ duration: 1 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport= {{ once: true }}
         className='flex flex-col items-center justify-center my-32'>
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>How it Works?</h1>
             <p className='text-lg text-gray-600 mb-8'>Transform Word Into Stunning Images</p>
