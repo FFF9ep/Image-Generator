c0003b5b0ee9daee6ee7adb470f1a93c022fa24f
fandi a <fandiiardy@gmail.com>
2024-12-06 19:02:21 +0700

    Update

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { motion } from 'motion/react'
 
 const Footer = () => {
     return (
-        <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+        <motion.div 
+        initial={{ opacity: 0.2, y: 100 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className='flex items-center justify-between gap-4 py-3 mt-20'>
             <img src={assets.logo} alt="" width={150} />
             <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright 2024 @Fandi  | All right reserved.</p>
             <div className='flex gap-2.5'>
@@ -11,7 +17,7 @@ const Footer = () => {
                 <img src={assets.twitter_icon} alt="" width={35} />
                 <img src={assets.instagram_icon} alt="" width={35} />
             </div>
-        </div>
+        </motion.div>
     )
 }
 
