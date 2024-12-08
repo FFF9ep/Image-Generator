2ce9393bc98d433b97eb31e39f470fc4988e77e7
fandi a <fandiiardy@gmail.com>
2024-12-08 13:26:07 +0700

    Updatre

--- Server/Config/MongoDb.js
+++ Server/Config/MongoDb.js
@@ -1,5 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
 const connectDB = async () => {
+
+    mongoose.connection.on('Connected', () => {
+        console.log('Connected to MongoDB')
+    })
+
     await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
 }
+
+export default connectDB;
