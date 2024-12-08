5a45cf5571a1df291ad234ece30a678be4938ce8
fandi a <fandiiardy@gmail.com>
2024-12-08 20:41:58 +0700

    Update generate image controller

--- Server/Controllers/ImageController.js
+++ Server/Controllers/ImageController.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import userModel from "../Models/UserModel";
+import FormData from 'form-data';
 
 
 export const generateImage = async (req, res) => {
@@ -14,7 +16,22 @@ export const generateImage = async (req, res) => {
             return res.json({success: false, message: 'You have no credits left!', creditBalance: user.creditBalance})
         }
 
-        
+        const formData = new FormData();
+        formData.append('prompt', prompt);
+
+        const {data} = await axios.post('https://clipdrop-api.co/text-to-image/v1', formData, {
+            headers: {
+                'x-api-key': process.env.CLIPDROP_API_KEY,
+            },
+            responseType: 'arraybuffer'
+        })
+
+        const base64Image = Buffer.from(data, 'binary').toString('base64');
+        const resultImage = `data:image/png;base64,${base64Image}`;
+
+        await userModel.findByIdAndUpdate(user._id, { creditBalance:user.creditBalance -1 })
+
+        res.json({success: true, message: "Image Successfully Generated!", creditBalance: user.creditBalance -1, resultImage })
 
     } catch (error) {
         console.log(error);
