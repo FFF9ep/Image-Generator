074bc9d532ca366680cd2c4cc8fdb0efe6df8923
fandi a <fandiiardy@gmail.com>
2024-12-08 15:42:21 +0700

    Update user controller

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -11,7 +11,22 @@ const registerUser = async (req, res) => {
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
+
+        const userData = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        const newUser = new userModel(userData);
+        const user = await newUser.save();
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY, {expiresIn: "1d"})
+
+        res.json({success:true, token, user: {name: user.name}})
+
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false, message: error.message})
     }
 }
