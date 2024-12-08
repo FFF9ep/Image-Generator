80699b116d4fb23def29662983161744ec109ac6
fandi a <fandiiardy@gmail.com>
2024-12-08 20:26:04 +0700

    add generate funt

--- Server/Controllers/ImageController.js
+++ Server/Controllers/ImageController.js
@@ -1,8 +1,21 @@
+import userModel from "../Models/UserModel";
 
 
 export const generateImage = async (req, res) => {
     try {
+        const { userId, prompt } = req.body;
+        const user = await userModel.findById(userId);
+
+        if (!user || !prompt) {
+            return res.json({success: false, message: 'Missing Details!'})
+        }
+
+        if (user.creditBalance === 0 || userModel.creditBalance < 0) {
+            return res.json({success: false, message: 'You have no credits left!', creditBalance: user.creditBalance})
+        }
+
         
+
     } catch (error) {
         console.log(error);
         res.json({success: false, message: error.message})
