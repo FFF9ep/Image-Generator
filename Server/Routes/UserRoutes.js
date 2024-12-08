c4badc2ec18469b88e5ab1701bfaf8c8e87b5231
fandi a <fandiiardy@gmail.com>
2024-12-08 15:53:25 +0700

    add routes

---
+++ Server/Routes/UserRoutes.js
@@ -0,0 +1,9 @@
+import { registerUser, loginUser } from "../Controllers/UserController.js";
+import express from 'express';
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+export default router;
