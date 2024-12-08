f206b559b7223ec40e8b60522bedd8940bc405bb
fandi a <fandiiardy@gmail.com>
2024-12-08 20:20:26 +0700

    add image controller

---
+++ Server/Controllers/ImageController.js
@@ -0,0 +1,10 @@
+
+
+export const generateImage = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
