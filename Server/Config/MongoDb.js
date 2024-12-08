f22f4c3952fd507c174597139ed9f08590f366ba
fandi a <fandiiardy@gmail.com>
2024-12-08 13:30:43 +0700

    Fixing some bugs

--- Server/Config/MongoDb.js
+++ Server/Config/MongoDb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
 
-    mongoose.connection.on('Connected', () => {
+    mongoose.connection.on('connected', () => {
         console.log("Connected to MongoDB")
     })
 
