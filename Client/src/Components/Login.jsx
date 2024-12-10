dd642e3b7e7a4a76544c98cb2f168706e06d47e7
fandi a <fandiiardy@gmail.com>
2024-12-10 18:52:51 +0700

    update

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const [state, setState] = useState('Login');
   const {setShowLogin} = useContext(AppContext);
 
-  const [name, setname] = useState('');
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -34,15 +34,15 @@ const Login = () => {
 
         {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
             <img src={assets} alt="" />
-            <input type="text" className='outline-none text-sm' placeholder='Full Name' required />
+            <input onChange={e => setName(e.target.value)} value={name} type="text" className='outline-none text-sm' placeholder='Full Name' required />
         </div>}
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
             <img src={assets.email_icon} alt="" />
-            <input type="email" className='outline-none text-sm' placeholder='Email id' required />
+            <input onChange={e => setEmail(e.target.value)} value={email} type="email" className='outline-none text-sm' placeholder='Email id' required />
         </div>
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
             <img src={assets.lock_icon} alt="" />
-            <input type="password" className='outline-none text-sm' placeholder='Password' required />
+            <input onChange={e => setPassword(e.target.value)} value={password} type="password" className='outline-none text-sm' placeholder='Password' required />
         </div>
 
         <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
