80ea97a5186031d5b4252ba1f223326bd9978a21
fandi a <fandiiardy@gmail.com>
2024-12-10 20:15:55 +0700

    update schema

--- Server/Models/TransactionModel.js
+++ Server/Models/TransactionModel.js
@@ -1,9 +1,32 @@
 import mongoose from 'mongoose';
 
 const transactionSchema = new mongoose.Schema({
-    
+    userId: {
+        type: String,
+        required: true
+    },
+    credits: {
+        type: Number,
+        required: true
+    },
+    plan: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    payment: {
+        type: Boolean,
+        default: false
+    },
+    date: {
+        type: Number
+    }
 })
 
-const transactionModel = mongoose.models.user || mongoose.model("transaction", transactionSchema)
+const transactionModel = mongoose.models.transaction || mongoose.model("transaction", transactionSchema)
 
-export default userModel;
+export default transactionModel;
+
